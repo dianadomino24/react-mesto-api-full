d1929499f461d81e3c0d5aab0e3910b8466b7d8b
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-12-10 16:27:21 +0300

    fixing register without s

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -44,7 +44,7 @@ const createUser = (req, res, next) => {
         throw new ConflictError('This user already exists');
       }
       console.log(SALT_ROUND)
-      return bcrypt.hash(password, SALT_ROUND);
+      return bcrypt.hash(password, 10);
     })
     .then((hash) => User.create({
       email,
